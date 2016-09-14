621fc580c5154f53c9cb72943a38a0b5a7272138
J. Ryan Conklin and Stephen Burden <j.@>
2016-09-14 15:07:21 -0700

    compine all functions into one findAndReplace function

--- src/FindReplace.php
+++ src/FindReplace.php
@@ -3,23 +3,64 @@
     class FindReplace
     {
 
+        // function findAndReplace($user_string, $find, $replace)
+        // {
+        //     // $position = strpos($user_string, $find);
+        //     $words = explode(" ", $user_string);
+        //     // $user_string_characters = str_split($words);
+        //     // $replace_characters = str_split($replace);
+        //     $found = 0;
+        //     if ($replace != "") {
+        //         for ($i=0; $i < count($words); $i++) {
+        //             if ($words[$i] == $find) {
+        //                 $words[$i] = $replace;
+        //                 $found += 1;
+        //             }
+        //         }
+        //         if ($found > 0) {
+        //             $words = implode(" ", $words);
+        //             return $words;
+        //         } else {
+        //             return "Word Not Found";
+        //         }
+        //     }
+        //     return $user_string;
+        // }
+
+
+        // function findAndReplace($user_string, $find, $replace)
+        // {
+        //     $output = str_ireplace($find, $replace, $user_string);
+        //     return $output;
+        // }
+
+        // function findAndReplacePartial($user_string, $find, $replace)
+        // {
+        //     $j = strpos($user_string, $find);
+        //     //$words = explode(" ", $user_string);
+        //     $user_string_characters = str_split($user_string);
+        //     $replace_characters = str_split($replace);
+        //     for ($i=0, $j; $j < $j + strlen($replace); $i++, $j++) {
+        //         $user_string_characters[$j] = $replace_characters[$i];
+        //     }
+        //     $words = implode("", $user_string_characters);
+        //     return $words;
+        // }
+
         function findAndReplace($user_string, $find, $replace)
         {
-
             $words = explode(" ", $user_string);
-            //$matches = array();
-
+            $found = 0;
             if ($replace != "") {
-                for ($i=0; $i < count($words); $i++) {
-                    if ($words[$i] == $find) {
-                        $words[$i] = $replace;
-                    }
+                if (strpos($user_string, $find) === false) {
+                    return "Word Not Found";
+                } else {
+                    $output = str_replace($find, $replace, $user_string);
+                    return $output;
                 }
+            } else {
+                return $user_string;
             }
-            $words = implode(" ", $words);
-            return $words;
-
         }
     }
-
 ?>
--- tests/FindReplaceTest.php
+++ tests/FindReplaceTest.php
@@ -2,16 +2,16 @@
     require_once "src/FindReplace.php";
     class FindReplaceTest extends PHPUnit_Framework_TestCase
     {
-        function test_findAndReturn_findAndReturn()
+        function test_findAndReplace_findAndReturn()
         {
             //Arrange
             $test_FindReplace = new FindReplace;
-            $input1 = "Hello World";
-            $input2 = "World";
-            $input3 = "";
+            $user_string = "Hello World";
+            $find = "World";
+            $replace = "";
 
             //Act
-            $output = $test_FindReplace->findAndReplace($input1, $input2, $input3);
+            $output = $test_FindReplace->findAndReplace($user_string, $find, $replace);
 
             //Assert
             $this->assertEquals("Hello World", $output);
@@ -21,18 +21,46 @@
         {
             //Arrange
             $test_FindReplace = new FindReplace;
-            $input1 = "Hello World";
-            $input2 = "World";
-            $input3 = "Universe";
+            $user_string = "Hello World";
+            $find = "World";
+            $replace = "Universe";
 
             //Act
-            $output = $test_FindReplace->findAndReplace($input1, $input2, $input3);
+            $output = $test_FindReplace->findAndReplace($user_string, $find, $replace);
 
             //Assert
             $this->assertEquals("Hello Universe", $output);
         }
 
+        function test_findAndReplace_wordNotFound()
+        {
+            //Arrange
+            $test_FindReplace = new FindReplace;
+            $user_string = "Hello World";
+            $find = "Dog";
+            $replace = "Universe";
 
+            //Act
+            $output = $test_FindReplace->findAndReplace($user_string, $find, $replace);
+
+            //Assert
+            $this->assertEquals("Word Not Found", $output);
+        }
+
+        function test_findAndReplacePartial_findAndReplacePartialWords()
+        {
+            //Arrange
+            $test_FindReplace = new FindReplace;
+            $user_string = "Hello World";
+            $find = "Worl";
+            $replace = "Lor";
+
+            //Act
+            $output = $test_FindReplace->findAndReplace($user_string, $find, $replace);
+
+            //Assert
+            $this->assertEquals("Hello Lord", $output);
+        }
 
 
     }
